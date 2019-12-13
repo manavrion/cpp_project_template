b42c54ba93de4fca01eab2e9a763de795430c798
Ruslan Manaev <manavrion@gmail.com>
2019-12-13 16:58:28 +0300

    Add build mode

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,3 +1,4 @@
+# Tools
 set(CMAKE_C_COMPILER clang-10)
 
 set(CMAKE_CXX_COMPILER clang++-10)
@@ -7,3 +8,6 @@ set(CMAKE_LINKER llvm-link-10)
 set(CMAKE_NM llvm-nm-10)
 set(CMAKE_OBJDUMP llvm-objdump-10)
 set(CMAKE_RANLIB llvm-ranlib-10)
+
+# Modes
+set(CMAKE_BUILD_TYPE Release)
