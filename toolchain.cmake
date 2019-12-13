71a98d1e0264db061dfdcb355e3f54bae24479fc
Ruslan Manaev <manavrion@gmail.com>
2019-12-13 16:53:50 +0300

    Add toolchain.cmake file

---
+++ toolchain.cmake
@@ -0,0 +1,9 @@
+set(CMAKE_C_COMPILER clang-10)
+
+set(CMAKE_CXX_COMPILER clang++-10)
+
+set(CMAKE_AR llvm-ar-10)
+set(CMAKE_LINKER llvm-link-10)
+set(CMAKE_NM llvm-nm-10)
+set(CMAKE_OBJDUMP llvm-objdump-10)
+set(CMAKE_RANLIB llvm-ranlib-10)
