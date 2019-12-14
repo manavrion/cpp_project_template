93124a6e737d573fc5728c94ff4eea6427c7e6e1
Ruslan Manaev <manavrion@gmail.com>
2019-12-14 22:33:23 +0300

    Fix CI

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ cd "$(dirname "$0")"
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake -DGTEST_ROOT=submodules/googletest/build/install/ ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake -DGTEST_LIBRARY=../submodules/googletest/build/install/lib/libgtest.a -DGTEST_MAIN_LIBRARY=../submodules/googletest/build/install/lib/libgtest_main.a -DGTEST_INCLUDE_DIR=../submodules/googletest/build/install/include/ ..
 cmake --build .
 cmake -DCMAKE_INSTALL_PREFIX=install -P cmake_install.cmake
--- toolchain.cmake
+++ toolchain.cmake
@@ -1,13 +1,7 @@
-# Tools
-set(CMAKE_C_COMPILER clang-10)
+# Build type
+set(CMAKE_BUILD_TYPE Debug)
 
-set(CMAKE_CXX_COMPILER clang++-10)
-
-set(CMAKE_AR llvm-ar-10)
-set(CMAKE_LINKER llvm-link-10)
-set(CMAKE_NM llvm-nm-10)
-set(CMAKE_OBJDUMP llvm-objdump-10)
-set(CMAKE_RANLIB llvm-ranlib-10)
-
-# Modes
-set(CMAKE_BUILD_TYPE Release)
+# C++
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
