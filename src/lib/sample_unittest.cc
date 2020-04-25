eaf53dea208c0d90426f42c2be0251292164a4c9
Ruslan Manaev <manavrion@gmail.com>
2020-04-25 17:48:06 +0500

    Add ranges-v3

--- src/lib/sample_unittest.cc
+++ src/lib/sample_unittest.cc
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 
 using template_repository::add;
+using template_repository::add_first3;
 using template_repository::mul;
 
 TEST(SampleTest, Add) {
@@ -13,4 +14,8 @@ TEST(SampleTest, Add) {
 TEST(SampleTest, Mul) {
   EXPECT_EQ(mul(1, 2), 2);
   EXPECT_EQ(mul(2, 2), 4);
+}
+
+TEST(SampleTest, AddFirst3) {
+  EXPECT_EQ(add_first3(std::vector<int64_t>{1, 1, 1, 5}), 3);
 }
