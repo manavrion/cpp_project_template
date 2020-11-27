0dff5d9adc86292eb0ab7437d2a395a7bb3e64db
Ruslan Manaev <manavrion@gmail.com>
2020-11-28 02:48:21 +0500

    Fix ci

--- .github/workflows/bootstrap.py
+++ .github/workflows/bootstrap.py
@@ -8,12 +8,15 @@ system = platform.system()
 print 'Selected toolchain - ' + toolchain
 print 'Run on - ' + system
 
+sys.stdout.flush()
+
 if system == 'Linux':
   if 'clang' in toolchain and 'gcc' in toolchain:
-    print('Broken toolchain!')
+    print 'Broken toolchain!'
     exit(1)
   if 'clang' in toolchain:
-    print('Install clang')
+    print 'Install clang'
+    sys.stdout.flush()
     os.system('wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -')
     os.system('apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic main"')
     os.system('apt update')
@@ -23,7 +26,8 @@ if system == 'Linux':
     os.system('apt install libc++-12-dev')
     os.system('apt install libc++abi-12-dev')
   if 'gcc' in toolchain:
-    print('Install gcc')
+    print 'Install gcc'
+    sys.stdout.flush()
     os.system('apt update')
     os.system('apt install gcc-10')
     os.system('apt install g++-10')
