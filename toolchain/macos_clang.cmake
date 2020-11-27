e9fc8c7345fef9f9dfbd5a1f627d286720a528e0
Ruslan Manaev <manavrion@gmail.com>
2020-11-28 03:33:47 +0500

    Fix ci

--- toolchain/macos_clang.cmake
+++ toolchain/macos_clang.cmake
@@ -2,5 +2,11 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/toolchain")
 include(default_toolchain)
 
 # Compiler
-set(CMAKE_C_COMPILER clang)
-set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_C_COMPILER clang-11)
+set(CMAKE_CXX_COMPILER clang++-11)
+
+set(CMAKE_AR llvm-ar-11)
+set(CMAKE_LINKER lld-11)
+set(CMAKE_NM llvm-nm-11)
+set(CMAKE_OBJDUMP llvm-objdump-11)
+set(CMAKE_RANLIB llvm-ranlib-11)
