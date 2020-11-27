592e13a22dcf2f9fa3bdcdcae8c73de02ece594b
Ruslan Manaev <manavrion@gmail.com>
2020-11-28 03:57:18 +0500

    Restore ranges

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cd "$(dirname "$0")"
-cmake -S . -B .build -DCMAKE_TOOLCHAIN_FILE=toolchain/default_toolchain.cmake
+cmake -S . -B .build -DCMAKE_TOOLCHAIN_FILE=toolchain/linux_clang.cmake
 cmake --build .build --parallel 8
--- src/lib/sample.cc
+++ src/lib/sample.cc
@@ -1,6 +1,7 @@
 #include "lib/sample.h"
 
-#include <numeric>
+#include <range/v3/numeric/accumulate.hpp>
+#include <range/v3/view/take.hpp>
 
 namespace template_repository {
 
@@ -8,9 +9,8 @@ int64_t add(const int64_t a, const int64_t b) { return a + b; }
 int64_t mul(const int64_t a, const int64_t b) { return a * b; }
 
 int64_t add_first3(const std::vector<int64_t>& xs) {
-  // return std::ranges::accumulate(xs | std::ranges::views::take(3),
-  // int64_t{});
-  return xs[0] + xs[1] + xs[2];
+  using namespace ranges;
+  return accumulate(xs | views::take(3), int64_t{});
 }
 
 }  // namespace template_repository
