aa8cbbc42e3d6349bbfd1de9e2b67ed1f6745928
Ruslan Manaev <manavrion@gmail.com>
2020-11-28 04:30:46 +0500

    Add header_only_library

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cd "$(dirname "$0")"
-cmake -S . -B .build -DCMAKE_TOOLCHAIN_FILE=toolchain/linux_clang.cmake
+cmake -S . -B .build -DCMAKE_TOOLCHAIN_FILE=toolchain/default_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
 cmake --build .build --parallel 8
---
+++ project_templates/header_only_library/include/manavrion/header_only_library_sample.h
@@ -0,0 +1,18 @@
+#ifndef MANAVRION_HEADER_ONLY_LIBRARY_SAMPLE_H_
+#define MANAVRION_HEADER_ONLY_LIBRARY_SAMPLE_H_
+
+#include <range/v3/numeric/accumulate.hpp>
+#include <range/v3/view/take.hpp>
+
+namespace header_only_library_sample {
+
+inline int64_t add(const int64_t a, const int64_t b) { return a + b; }
+inline int64_t mul(const int64_t a, const int64_t b) { return a * b; }
+
+inline int64_t add_first3(const std::vector<int64_t>& xs) {
+  return ranges::accumulate(xs | ranges::views::take(3), int64_t{});
+}
+
+}  // namespace header_only_library_sample
+
+#endif  // MANAVRION_HEADER_ONLY_LIBRARY_SAMPLE_H_
