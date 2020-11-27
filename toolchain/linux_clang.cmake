28b06b4ee3c871dc11996c9bd1863b84e17e010a
Ruslan Manaev <manavrion@gmail.com>
2020-11-28 03:12:00 +0500

    Make bootstrap only for linux

--- toolchain/linux_clang.cmake
+++ toolchain/linux_clang.cmake
@@ -2,5 +2,5 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/toolchain")
 include(default_toolchain)
 
 # Compiler
-set(CMAKE_C_COMPILER clang-9)
-set(CMAKE_CXX_COMPILER clang++-9)
+set(CMAKE_C_COMPILER clang-12)
+set(CMAKE_CXX_COMPILER clang++-12)
