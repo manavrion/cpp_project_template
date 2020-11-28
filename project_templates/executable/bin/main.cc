985fc02da4ace793fcd6be1a8868fef6102ed35b
Ruslan Manaev <manavrion@gmail.com>
2020-11-28 05:50:49 +0500

    Add executable

--- project_templates/executable/bin/main.cc
+++ project_templates/executable/bin/main.cc
@@ -1,15 +1,8 @@
 #include <iostream>
 
-#include "lib/sample.h"
-
-using template_repository::add;
-using template_repository::mul;
-
 int main() {
-  int64_t a, b;
+  int64_t a;
+  int64_t b;
   std::cin >> a >> b;
-
-  std::cout << "add: " << add(a, b) << std::endl;
-  std::cout << "mul: " << mul(a, b) << std::endl;
-  return 0;
+  std::cout << a + b << std::endl;
 }
