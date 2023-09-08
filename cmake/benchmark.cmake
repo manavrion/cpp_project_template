7c8cb3ca41c6f1cd11ca95a01039dabd7105b83d
Ruslan Manaev <manavrion@gmail.com>
2023-09-07 20:28:08 -0700

    Fix repo names

--- cmake/benchmark.cmake
+++ cmake/benchmark.cmake
@@ -2,7 +2,9 @@ include(FetchContent)
 
 set(BENCHMARK_ENABLE_TESTING OFF)
 
-FetchContent_Declare(benchmark
-                     GIT_REPOSITORY https://github.com/google/benchmark.git)
+FetchContent_Declare(
+  benchmark
+  GIT_REPOSITORY https://github.com/google/benchmark.git
+  GIT_TAG main)
 
 FetchContent_MakeAvailable(benchmark)
--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -5,7 +5,9 @@ set(gtest_force_shared_crt
     ON
     CACHE BOOL "" FORCE)
 
-FetchContent_Declare(googletest
-                     GIT_REPOSITORY https://github.com/google/googletest.git)
+FetchContent_Declare(
+  googletest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG main)
 
 FetchContent_MakeAvailable(googletest)
