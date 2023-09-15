7161c7db0a54f7c41b810841ac54e3bf16bca507
Ruslan Manaev <manavrion@gmail.com>
2023-09-15 00:30:55 +0000

    Enable ASAN builds

---
+++ toolchain/ubuntu_llvm_asan.cmake
@@ -0,0 +1,15 @@
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/toolchain")
+include(ubuntu_llvm)
+
+# ASAN flags:
+# Reference https://clang.llvm.org/docs/AddressSanitizer.html#usage
+# -fsanitize=address enables ASAN.
+# -fno-omit-frame-pointer produces nicer stack traces in error messages.
+# -fno-optimize-sibling-calls disables tail call optinization.
+set(CPP_PROJECT_TEMPLATE_ASAN_FLAGS "-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls")
+
+set(CMAKE_C_FLAGS_INIT "${CPP_PROJECT_TEMPLATE_ASAN_FLAGS}")
+set(CMAKE_CXX_FLAGS_INIT "${CPP_PROJECT_TEMPLATE_ASAN_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "${CPP_PROJECT_TEMPLATE_ASAN_FLAGS}")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "${CPP_PROJECT_TEMPLATE_ASAN_FLAGS}")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "${CPP_PROJECT_TEMPLATE_ASAN_FLAGS}")
