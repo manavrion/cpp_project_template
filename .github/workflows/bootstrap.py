6e5b0840f088d6b6949fd2653ee5f486b7704bf2
Ruslan Manaev <manavrion@gmail.com>
2023-09-17 04:00:46 +0000

    Fix CI

--- .github/workflows/bootstrap.py
+++ .github/workflows/bootstrap.py
@@ -38,8 +38,3 @@ if system == 'Darwin':
     print('Install ninja-build')
     sys.stdout.flush()
     os.system('brew install ninja')
-if system == 'Windows':
-    print('Install ninja-build')
-    sys.stdout.flush()
-    os.system('choco -y install ninja')
-    os.system('choco -y install visualstudio2022buildtools')
