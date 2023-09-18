b5fab0893faa994edb3c1389eaf1d61a1849945f
Ruslan Manaev <manavrion@gmail.com>
2023-09-18 07:22:30 +0000

    Fix MSAN

--- scripts/bootstrap_msan.sh
+++ scripts/bootstrap_msan.sh
@@ -8,13 +8,13 @@ SCRIPTPATH=`dirname $SCRIPT`
 
 cd $SCRIPTPATH
 
-git clone --depth=1 https://github.com/llvm/llvm-project
+git clone --depth=1 -b llvmorg-17.0.0-rc4 https://github.com/llvm/llvm-project
 cd llvm-project
 
 cmake -G Ninja \
     -S runtimes \
 	-B build \
-    -DCMAKE_TOOLCHAIN_FILE=$SCRIPTPATH/../toolchain/ubuntu_llvm_msan.cmake \
+    -DCMAKE_TOOLCHAIN_FILE=$SCRIPTPATH/../toolchain/ubuntu_llvm.cmake \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DLLVM_USE_SANITIZER=MemoryWithOrigins \
 	-DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi"
