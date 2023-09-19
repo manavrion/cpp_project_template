f0eefe155a94b284741a2da9a98a4f6388e03daa
Ruslan Manaev <manavrion@gmail.com>
2023-09-19 06:33:16 +0000

    Remove ccache

--- cmake/ccache.cmake
+++
@@ -1,9 +0,0 @@
-message("-- Detecting CCache")
-find_program(CCACHE_PROGRAM ccache)
-
-if(CCACHE_PROGRAM)
-  set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
-  message("-- Detecting CCache - done")
-else()
-  message("-- Detecting CCache - failed")
-endif()
