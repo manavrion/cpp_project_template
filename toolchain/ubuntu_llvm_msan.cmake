6d1c0bf7e865513bfd9f534d455fb245810480d0
Ruslan Manaev <manavrion@gmail.com>
2023-09-19 01:54:50 +0000

    Update cmake

--- toolchain/ubuntu_llvm_msan.cmake
+++ toolchain/ubuntu_llvm_msan.cmake
@@ -16,32 +16,32 @@ set(CPP_PROJECT_TEMPLATE_MSAN_FLAGS
 # https://github.com/google/sanitizers/wiki/MemorySanitizerLibcxxHowTo#instrumented-gtest
 # we need to use an instrumented libc++.
 
-# Using a custom built libc++:
+# Using an instrumented libc++:
 # https://github.com/llvm/llvm-project/blob/main/libcxx/docs/UsingLibcxx.rst#using-a-custom-built-libc
 
-set(CPP_PROJECT_TEMPLATE_INSTRUMENTED_CPP_COMPILER_FLAGS
+set(CPP_PROJECT_TEMPLATE_INSTRUMENTED_LIBCPP_COMPILER_FLAGS
     "-nostdinc++ -nostdlib++ -isystem /workspaces/cpp_project_template/scripts/llvm-project/build/include/c++/v1"
 )
 
-set(CPP_PROJECT_TEMPLATE_INSTRUMENTED_CPP_LINKER_FLAGS
+set(CPP_PROJECT_TEMPLATE_INSTRUMENTED_LIBCPP_LINKER_FLAGS
     "-nostdinc++ -nostdlib++ -isystem /workspaces/cpp_project_template/scripts/llvm-project/build/include/c++/v1 -L /workspaces/cpp_project_template/scripts/llvm-project/build/lib -Wl,-rpath,/workspaces/cpp_project_template/scripts/llvm-project/build/lib -lc++"
 )
 
 set(CMAKE_C_FLAGS_INIT
-    "${CPP_PROJECT_TEMPLATE_MSAN_FLAGS} ${CPP_PROJECT_TEMPLATE_INSTRUMENTED_CPP_COMPILER_FLAGS}"
+    "${CPP_PROJECT_TEMPLATE_MSAN_FLAGS} ${CPP_PROJECT_TEMPLATE_INSTRUMENTED_LIBCPP_COMPILER_FLAGS}"
 )
 set(CMAKE_CXX_FLAGS_INIT
-    "${CPP_PROJECT_TEMPLATE_MSAN_FLAGS} ${CPP_PROJECT_TEMPLATE_INSTRUMENTED_CPP_COMPILER_FLAGS}"
+    "${CPP_PROJECT_TEMPLATE_MSAN_FLAGS} ${CPP_PROJECT_TEMPLATE_INSTRUMENTED_LIBCPP_COMPILER_FLAGS}"
 )
 
 set(CMAKE_EXE_LINKER_FLAGS_INIT
-    "${CPP_PROJECT_TEMPLATE_MSAN_FLAGS} ${CPP_PROJECT_TEMPLATE_INSTRUMENTED_CPP_LINKER_FLAGS}"
+    "${CPP_PROJECT_TEMPLATE_MSAN_FLAGS} ${CPP_PROJECT_TEMPLATE_INSTRUMENTED_LIBCPP_LINKER_FLAGS}"
 )
 set(CMAKE_MODULE_LINKER_FLAGS_INIT
-    "${CPP_PROJECT_TEMPLATE_MSAN_FLAGS} ${CPP_PROJECT_TEMPLATE_INSTRUMENTED_CPP_LINKER_FLAGS}"
+    "${CPP_PROJECT_TEMPLATE_MSAN_FLAGS} ${CPP_PROJECT_TEMPLATE_INSTRUMENTED_LIBCPP_LINKER_FLAGS}"
 )
 set(CMAKE_SHARED_LINKER_FLAGS_INIT
-    "${CPP_PROJECT_TEMPLATE_MSAN_FLAGS} ${CPP_PROJECT_TEMPLATE_INSTRUMENTED_CPP_LINKER_FLAGS}"
+    "${CPP_PROJECT_TEMPLATE_MSAN_FLAGS} ${CPP_PROJECT_TEMPLATE_INSTRUMENTED_LIBCPP_LINKER_FLAGS}"
 )
 
 # Bypass the following error: -- Performing Test HAVE_POSIX_REGEX -- compiled
