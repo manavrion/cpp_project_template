eff9cf3571b992586112586fbce7c079972f20eb
Ruslan Manaev <manavrion@gmail.com>
2023-09-19 03:06:00 +0000

    Remove debug flag

--- cmake/benchmark.cmake
+++ cmake/benchmark.cmake
@@ -2,8 +2,6 @@ include(FetchContent)
 
 set(BENCHMARK_ENABLE_TESTING OFF)
 
-set(CXXFEATURECHECK_DEBUG ON)
-
 FetchContent_Declare(
   benchmark
   GIT_REPOSITORY https://github.com/google/benchmark.git
