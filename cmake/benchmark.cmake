ba0065aeee7d252d272fde1a2a4a9f98ed164d9f
Ruslan Manaev <manavrion@gmail.com>
2023-09-19 02:43:44 +0000

    Enable CXXFEATURECHECK_DEBUG on google benchmark

--- cmake/benchmark.cmake
+++ cmake/benchmark.cmake
@@ -2,6 +2,8 @@ include(FetchContent)
 
 set(BENCHMARK_ENABLE_TESTING OFF)
 
+set(CXXFEATURECHECK_DEBUG ON)
+
 FetchContent_Declare(
   benchmark
   GIT_REPOSITORY https://github.com/google/benchmark.git
