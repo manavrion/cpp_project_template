135ba11dcd5899eca79d7ba08601f79a898fc3ef
Ruslan Manaev <manavrion@gmail.com>
2023-09-19 03:54:56 +0000

    Enable TSAN

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cd "$(dirname "$0")"
-cmake -G Ninja -S . -B build -DCMAKE_TOOLCHAIN_FILE=toolchain/ubuntu_llvm_msan.cmake -DCMAKE_BUILD_TYPE=Debug
+cmake -G Ninja -S . -B build -DCMAKE_TOOLCHAIN_FILE=toolchain/ubuntu_llvm.cmake -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
